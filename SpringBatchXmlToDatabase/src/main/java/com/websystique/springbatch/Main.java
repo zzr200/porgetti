19be335f71dd46eb6e8273c03cf3bcd03ab562b9
Rocco Demasi <rocco.demasi@inwind.it>
2020-11-01 08:11:59 +0100

    Aggiunta stringa eccezione

---
+++ SpringBatchXmlToDatabase/src/main/java/com/websystique/springbatch/Main.java
@@ -0,0 +1,39 @@
+package com.websystique.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionException;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	@SuppressWarnings("resource")
+	public static void main(String areg[]){
+		
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-batch-context.xml");
+		
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("examResultJob");
+	 
+		try {
+			JobExecution execution = jobLauncher.run(job, new JobParameters());
+			System.out.println("Eccezione Job Exit Status : "+ execution.getStatus());
+	 
+		} catch (JobExecutionException e) {
+			System.out.println("Job ExamResult failed");
+			e.printStackTrace();
+		}
+	}
+
+}
+
+/*
+ * Right-click on your project
+	Click Properties
+	Java build path: Libraries; Remove the "JRE System Library[J2SE 1.5]"
+	Click Add Library -> JRE System Library
+ */
